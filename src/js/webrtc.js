7da4095b39c223d024053193352dfdd7979cdd7f
Alexander Kaplinskiy <kav661@gmail.com>
2025-10-18 09:14:03 +0300

    test async

--- src/js/webrtc.js
+++ src/js/webrtc.js
@@ -226,19 +226,20 @@
     } catch {}
     pc = new RTCPeerConnection(cfg);
         // Ensure offer is created when negotiation is needed
-
+    pc.onnegotiationneeded = async () => {
+      try { await sendOfferIfPossible(true); } catch {}
     };
     // Ensure offer is created when negotiation is needed (debounced to avoid double fires)
-    pc.onnegotiationneeded = async () => {
-      const r = __getRole();
-      //if (r !== 'caller') return; // only caller initiates offer
-      //if (typeof window !== 'undefined' && window.__OFFER_SENT__) return;
-      //if (negotiationScheduled) return;
-      //negotiationScheduled = true;
-      //setTimeout(async () => {
-        try { await sendOfferIfPossible(); } catch {} //finally { negotiationScheduled = false; }
-      //}, 300);
-    };
+    //pc.onnegotiationneeded = () => {
+    //  const r = __getRole();
+    //  if (r !== 'caller') return; // only caller initiates offer
+     // if (typeof window !== 'undefined' && window.__OFFER_SENT__) return;
+     // if (negotiationScheduled) return;
+    //  negotiationScheduled = true;
+    //  setTimeout(async () => {
+     //   try { await sendOfferIfPossible(); } catch {} finally { negotiationScheduled = false; }
+    //  }, 300);
+    //};
 
     // Extra diagnostics
     pc.onicecandidateerror = (e) => {
